8456aef3b9b79c09fdcbc68b0cda39f98c8bfb9c
EDWIN JACKSON <100138857@student.georgianc.on.ca>
2019-04-07 17:46:16 -0400

    added skill section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Layout from '../components/Layout';
 import About from '../components/container/about';
 import Experience from '../components/container/experience';
+import Skills from '../components/container/skills';
 
 const HomePage = () => (
   <Layout>
     <About />
     <Experience />
+    <Skills />
   </Layout>
 );
 
