66a661f4201a2187197edff8002453f86325d042
EDWIN JACKSON <100138857@student.georgianc.on.ca>
2019-05-19 20:49:42 -0400

    totally forgot the onclick needed to be wired up in the child

--- src/components/x.js
+++ src/components/x.js
@@ -13,9 +13,10 @@ const SVG = styled.svg`
   }
 `;
 
-const X = ({open}) => (
+const X = ({open, onClick}) => (
   <SVG 
     open={open}
+    onClick={onClick}
     xmlns="http://www.w3.org/2000/svg" 
     width="24" 
     height="24" 
